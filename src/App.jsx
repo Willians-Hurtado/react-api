cd6bb195fcbd12efc66418ab34412bcc75337724
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-28 18:33:36 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,26 +16,29 @@ function App() {
 
   return (
     <>
-      <div className='container mt-5 mb-5 text-center text-light bg-dark p-5 rounded shadow border border-light border-5'>
+      <div className=' text-center text-light bg-dark p-5  '>
         <h1>Post List</h1>
 
       </div>
 
       {
-        <table className='table table-striped table-bordered table-dark' >
+        <table className='table table-striped table-bordered table-dark mt-5 ' >
           <thead>
             <tr>
               <th>Title</th>
-              <th>Slug</th>
               <th>Content</th>
+              <th>Image</th>
+
             </tr>
           </thead>
-          <tbody>
+          <tbody className=' '>
             {postList.map((post) => (
               <tr key={post.id}>
-                <td>{post.title}</td>
-                <td>{post.slug}</td>
+                <td >{post.title}</td>
                 <td>{post.content}</td>
+                <td>
+                  <img src={`http://localhost:3009/images/${post.image}`} alt={post.title} style={{ width: '250px', height: '100%' }} />
+                </td>
               </tr>
             ))}
           </tbody>
