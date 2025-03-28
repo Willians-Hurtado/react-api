b3e5ca4b5b7d94fbf9b59c42d6ab9ae1e916a58c
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-28 17:11:49 +0100

    created React app and fetched posts Added useEffect and displayed posts in a table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [postList, setPostList] = useState([]);
+
+  function fetchPostList() {
+
+    fetch("http://localhost:3009/api/v1/posts")
+      .then((res) => res.json())
+      .then(setPostList);
+
+  }
+  useEffect(fetchPostList, []);
+
 
   return (
     <>
+      {
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Slug</th>
+              <th>Content</th>
+            </tr>
+          </thead>
+          <tbody>
+            {postList.map((post) => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>{post.slug}</td>
+                <td>{post.content}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
+
+
+      }
     </>
   )
 }
